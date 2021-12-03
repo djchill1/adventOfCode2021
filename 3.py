e22057a0043344dac0a47507d2bfcb23bc71c80d
djchill1 <djchill10@googlemail.com>
2021-12-03 13:13:30 +0000

    day 3 complete

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 import init
 
-data = init.read_data(True,)
+data = init.read_data(False,)
 
 
 def sums(data):
@@ -58,31 +58,87 @@ def part1():
 	return gamma * epsilon
 
 
-def part2():
-	max = len(data[0])
+def delete_from_list(input, value, bit):
+	result = []
+	for entry in input:
+		# print(entry)
+		entry = str(entry)
+		if entry[bit] == str(value):
+			result.append(entry)
 
-	most = data
-	least = data
+	return result
 
-	# oxygen
-	for i in range(0, max):
-		interim, checker = sums(most)
 
-		for index, elem in enumerate(interim):
-			if elem >= checker[index]:
-				# most common element is 1:
-				comm = 1
+def list_reducer(input, most = True):
+	byte_length = len(input[0])
+	if most:
+		round_up = True
+	else:
+		round_up = False
+
+	bit = 0
+	while True:
+
+		number_of_values = len(input)
+		count = 0
+
+		print('checking bit', bit, 'for input', input)
+
+		# check if last element
+		if number_of_values == 1:
+			return input[0]
+
+		# find how many 1s
+		for i in input:
+			# ith element value
+			if str(i)[bit] == '1':
+				count += 1
+			print(i, count)
+
+		if count / number_of_values > 0.5:
+			if round_up:
+				# 1 wins
+				input = delete_from_list(input, 1, bit)
+				print('1 wins')
 			else:
-				comm = 0
+				# 0 wins
+				input = delete_from_list(input, 0, bit)
+				print('0 wins')
+		elif count / number_of_values < 0.5:
+			if round_up:
+				# 0 wins
+				input = delete_from_list(input, 0, bit)
+				print('0 wins')
+			else:
+				# 1 wins
+				input = delete_from_list(input, 1, bit)
+				print('1 wins')
+		else:
+			# round correctly
+			if round_up:
+				input = delete_from_list(input, 1, bit)
+				print('1 wins')
+			else:
+				input = delete_from_list(input, 0, bit)
+				print('0 wins')
 
-		new_most = []
-		for number in most:
-			if number[i] == comm:
-				new_most.append(number)
-		most = new_most
-	print(most)
+		bit += 1
 
-	# return oxygen * co2
+
+
+
+def part2():
+	oxy_possibles = data
+
+
+	oxygen = list_reducer(data, True)
+	print('CO2 NOW *******************************************')
+	co2 = list_reducer(data, False)
+
+	oxygen = int(oxygen, 2)
+	co2 = int(co2, 2)
+
+	return int(oxygen) * int(co2)
 
 
 print(f'Part 1: {part1()}, Part 2: {part2()}')
