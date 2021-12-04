a1b65c88bae8e2b901ba80c93b378e980e3adaba
djchill1 <djchill10@googlemail.com>
2021-12-04 12:03:46 +0000

    day 4 complete

--- 4.py
+++ 4.py
@@ -1,49 +1,66 @@
 import init
 import numpy as np
+import re
 
-data = init.read_data(True, )
+data = init.read_data(False, )
 numbers = data[0].split(',')
 
-def draw_number(index, boards):
-	drawn_number = numbers[index]
-	print('drawing number', drawn_number)
-	bingo = False
-	for line in boards:
-		# print(line)
-		for number in line:
-			# print(number)
-			number = str(number)
-			if number == drawn_number:
-				line.remove(int(number))
-				# print('removed', number, line)
-		if len(line) == 0:
-			print('BINGO')
-			bingo = True
-	return boards, bingo, drawn_number
-
 def part1():
-	boards = [list(map(int, x.split())) for x in data[1::]]
-	for entry in boards:
-		if entry == []:
-			boards.remove([])
+	boards = list()
+	for line in [list(map(int, x.split())) for x in data[1::]]:
+		if len(line) == 5:
+			boards[-1].extend([int(i) for i in line])
+		else:
+			boards.append(list())
+
 	# print(boards)
-	i = 0
-	bingo = False
-	while not bingo:
-		boards, bingo, drawn_number = draw_number(i, boards)
-		print(boards)
-		i += 1
 
-	# find winning board
-	loc = boards.index([])
-	print(loc)
-
-	winning_sum = 1
-	return winning_sum * drawn_number
+	for drawn_number in numbers:
+		drawn_number = int(drawn_number)
+		print('playing number', drawn_number)
+		for board in boards:
+			# print(board)
+			try:
+				board[board.index(drawn_number)] = -1
+				# print('after', board)
+				for i in range(0,5):
+					if sum(board[i::5]) == -5 or sum(board[i * 5:][:5]) == -5:
+						print("BINGO")
+						score = str(drawn_number * sum([i if i>-1 else 0 for i in board]))
+						return score
+			except:
+				pass
 
 
 def part2():
-	return False
+	boards = list()
+	for line in [list(map(int, x.split())) for x in data[1::]]:
+		if len(line) == 5:
+			boards[-1].extend([int(i) for i in line])
+		else:
+			boards.append(list())
+
+	# print(boards)
+	numWins = 0
+
+	for drawn_number in numbers:
+		drawn_number = int(drawn_number)
+		print('playing number', drawn_number)
+		for board in boards:
+			# print(board)
+			try:
+				board[board.index(drawn_number)] = -1
+				# print('after', board)
+				for i in range(0, 5):
+					if sum(board[i::5]) == -5 or sum(board[i * 5:][:5]) == -5:
+						if numWins == len(boards)-1:
+							print("BINGO")
+							score = str(drawn_number * sum([i if i > -1 else 0 for i in board]))
+							return score
+						numWins += 1
+						board.clear()
+			except:
+				pass
 
 
 print(f'Part 1: {part1()}, Part 2: {part2()}')
