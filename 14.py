b0c5ce5344a75ac048d98e88cdfd383c5b778dc5
djchill1 <djchill10@googlemail.com>
2021-12-14 17:53:00 +0000

    day 14 ⭐

--- 14.py
+++ 14.py
@@ -1,6 +1,6 @@
 import init
 
-data = init.read_data(False, )
+data = init.read_data(True, )
 
 
 def part1(maximum):
@@ -11,8 +11,8 @@ def part1(maximum):
         appender = [line[0][0], line[0][1], line[2]]
         pair_rules.append(appender)
 
-    print(polymer_template)
-    print(pair_rules)
+    # print(polymer_template)
+    # print(pair_rules)
 
     count = 0
     while count < maximum:
@@ -46,14 +46,84 @@ def part1(maximum):
         # print(polymer_template)
         # print(polymer_counts)
 
-        all_values = polymer_counts.values()
-        max_value = max(all_values)
-        min_value = min(all_values)
+    all_values = polymer_counts.values()
+    max_value = max(all_values)
+    min_value = min(all_values)
+    print('part 1', polymer_counts)
     return max_value - min_value
 
 
-def part2():
-    return False
+def add_to_dict(dictionary, key, value=1):
+    if key not in dictionary:
+        dictionary[key] = value
+    else:
+        dictionary[key] += value
+    return dictionary
 
 
-print(f'Part 1: {part1(40)}, Part 2: {part2()}')
+def part2(maximum):
+    polymer_template = list(data[0])
+    pair_rules = []
+
+    for line in [list(map(str, x.split())) for x in data[2::]]:
+        appender = [line[0][0], line[0][1], line[2]]
+        pair_rules.append(appender)
+
+    polymer_pair_counts = {}
+
+    # initalise the pair counts
+    for index, value in enumerate(polymer_template):
+        if index == len(polymer_template) - 1:
+            # end value
+            break
+
+        next_value = polymer_template[index + 1]
+
+        pair = value + next_value
+
+        if pair not in polymer_pair_counts:
+            polymer_pair_counts[pair] = 1
+        else:
+            polymer_pair_counts[pair] += 1
+
+    print(polymer_pair_counts)
+
+    count = 0
+    while count < maximum:
+        new_polymer_pair_counts = {}
+        for (key, value) in polymer_pair_counts.items():
+            # print(key, value)
+
+            for line in pair_rules:
+                # print('checking', line[0] + line[1])
+                if key == line[0] + line[1]:
+                    new_pair = line[0] + line[2]
+                    print(key, 'is a MATCH! Adding in', new_pair)
+                    new_polymer_pair_counts = add_to_dict(new_polymer_pair_counts, new_pair)
+                    new_pair = line[2] + line[1]
+                    print('also adding in', new_pair)
+                    new_polymer_pair_counts = add_to_dict(new_polymer_pair_counts, new_pair)
+                    break
+
+        polymer_pair_counts = new_polymer_pair_counts
+        print(polymer_pair_counts)
+        count += 1
+
+    # pairs to values
+    letters = {}
+    for letter1, letter2 in polymer_pair_counts:
+        value = polymer_pair_counts[letter1 + letter2]
+        print(letter1, letter2, value)
+        letters = add_to_dict(letters, letter1, value)
+        letters = add_to_dict(letters, letter2, value)
+
+    all_values = letters.values()
+    print(letters)
+    max_value = max(all_values)
+    min_value = min(all_values)
+    return max_value - min_value
+    # return False
+
+
+n = 3
+print(f'Part 1: {part1(n)}, Part 2: {part2(n)}')
